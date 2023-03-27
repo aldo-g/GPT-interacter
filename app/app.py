2fea8b2a719bbd22bea92c3cf0d75d06c29d71ba
Ali-Personal <ali@Alastairs-MacBook-Pro.local>
2023-03-27 23:02:06 +0200

    working poooorly

--- app/app.py
+++ app/app.py
@@ -2,30 +2,28 @@ import httpx
 import asyncio
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI, File, UploadFile
-import docx
-import openai
-from typing import Optional
-from fastapi import FastAPI, File, UploadFile, HTTPException, Depends
+from fastapi import FastAPI, File, UploadFile, Request, HTTPException, Depends
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
-from fastapi.responses import FileResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from io import BytesIO
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+import logging
+import time
+import openai
+import docx
 
-
-
+logging.basicConfig(level=logging.DEBUG)
+templates = Jinja2Templates(directory="app/templates")
 
 app = FastAPI()
 
-app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def serve_title_page():
-    with open("frontend/markup/title.html", "r") as f:
-        content = f.read()
-    return content
+async def serve_title_page(request: Request):
+    return templates.TemplateResponse("title.html", {"request": request})
 
 load_dotenv()
 api_key = os.environ.get("OPENAI_API_KEY")
@@ -42,13 +40,15 @@ def read_docx(file):
         full_text.append(paragraph.text)
     return '\n'.join(full_text)
 
-def analyze_cv(cv_text: str) -> str:
+async def analyze_cv(cv_text: str) -> str:
     prompt = (
         f"Please provide an in-depth analysis of the following CV, covering at least two chapters. "
         f"Reference specific snippets from the candidate's CV, give special attention to their qualifications and previous experience, "
         f"This is to provide an overview of the candidate to a recruiting company, not an anylsis for a job position. You should however recomend roles that the candidate would be good for"
         f"and provide a rating out of 10 for the candidate:\n\n{cv_text}\n\nIn-depth analysis:"
-    )    
+    )
+    
+    start_time = time.time()
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
@@ -57,30 +57,35 @@ def analyze_cv(cv_text: str) -> str:
         stop=None,
         temperature=0.5,
     )
-
-    analysis_text = response.choices[0].text.strip()
-    analysis_text = analysis_text.replace("Quality analysis:", "").strip()
-
-    return analysis_text
+    end_time = time.time()
+    
+    logging.debug(f"Time taken for GPT-3 call: {end_time - start_time} seconds")
+    return response.choices[0].text.strip()
 
 
 class CVAnalysisResponse(BaseModel):
     cv_analysis: str
 
 @app.post("/upload_cv", response_model=CVAnalysisResponse)
-async def upload_cv(cv: UploadFile = File(...)):
+async def upload_cv(request: Request, cv: UploadFile = File(...)):
+    start_time = time.time()
     cv_text = read_docx(cv.file)
-    analysis_text = analyze_cv(cv_text)
+    end_time = time.time()
+    
+    logging.debug(f"Time taken to read docx: {end_time - start_time} seconds")
+    
+    start_time = time.time()
+    analysis_text = await analyze_cv(cv_text)
+    end_time = time.time()
+    
+    logging.debug(f"Time taken for analyze_cv: {end_time - start_time} seconds")
 
-    return templates.TemplateResponse("analysis.html", {"request": {"analysis_text": analysis_text}})
+    return templates.TemplateResponse("analysis.html", {"analysis_text": analysis_text, "request": request})
 
 @app.get("/analyze_cv/")
-async def analyze_cv_form():
-    return FileResponse("frontend/markup/analyze_cv.html")
+async def analyze_cv_form(request: Request):
+    return templates.TemplateResponse("analyze_cv.html", {"request": request})
 
-@app.get("/frontend/style/{filename}")
+@app.get("/static/style/{filename}")
 async def get_style(filename: str):
-    return FileResponse(f"frontend/style/{filename}")
-
-templates = Jinja2Templates(directory="path/to/your/templates_directory")
-
+    return FileResponse(f"app/static/style/{filename}")
